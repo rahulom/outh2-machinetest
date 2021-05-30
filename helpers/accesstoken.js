b9c777fec3cfb39b32f7ea91d6fe2c42c9078179
rahulom007 <rahulom666@gmail.com>
2021-05-30 17:53:37 +0530

    Removed the default setup vvalue

--- helpers/accesstoken.js
+++ helpers/accesstoken.js
@@ -3,14 +3,13 @@ const jwt=require('jsonwebtoken');
 
 
 function generateToken(payload,expiry){
-    //Not recommeneded, SECRET Values should be set via ENV
- return   jwt.sign(payload,process.env.SECRET||'test',{expiresIn:expiry});
+ return   jwt.sign(payload,process.env.SECRET,{expiresIn:expiry});
 }
 
 function validateToken(token){
     console.log({token});
     try {
-        return jwt.verify(payload,process.env.SECRET||'test');
+        return jwt.verify(token,process.env.SECRET);
     } catch (error) {
         console.log({error});
         return ;
